93aafbd0be11a5e701a274cbc16cb95f865c1586
Chryat1s <33789672+Chryat1s@users.noreply.github.com>
2018-01-06 18:11:14 +0100

    Erstellung einer Bucket Klasse

---
+++ engine/src/de/jhit/opendiabetes/vault/container/Bucket.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2018 aa80hifa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.jhit.opendiabetes.vault.container;
+
+/**
+ *
+ * @author aa80hifa
+ */
+public class Bucket {
+    
+    private VaultEntry vaultEntry;
+    
+    // Array size settings
+    private static final int numberOfEntries = 2;
+
+    // time countdown Array
+    private int[] timeCountDownArray;
+    // onehot Boolean
+    private boolean[] booleanArray;
+    // onehot trigger
+    private VaultEntryType[] entryTypeArray;
+    // TODO missing wait till next entry
+    private VaultEntryType[] findNextArray;
+
+
+    public Bucket(VaultEntry entry) {
+            vaultEntry = entry;
+            
+            // Arrays containing OneHot information
+            timeCountDownArray = new int[numberOfEntries];
+            booleanArray = new boolean[numberOfEntries];
+            entryTypeArray = new VaultEntryType[numberOfEntries];
+            findNextArray = new VaultEntryType[numberOfEntries];
+    }
+
+    //
+    // GETTER VaultEntry
+    //
+    public VaultEntry getVaultEntry() {
+            return vaultEntry;
+    }
+
+    //
+    // GETTER - SETTER onhotArraySize
+    //
+    public static int getNumberOfEntries() {
+            return numberOfEntries;
+    }
+
+    //
+    // GETTER
+    //
+    // TODO get time countdown
+    // ArrayOutOfBounds
+    public int getTimeCountDown(int position) {
+            return timeCountDownArray[position];
+    }
+    // TODO get boolean
+    // ArrayOutOfBounds
+    public Boolean getBoolean(int position) {
+            return booleanArray[position];
+    }
+    // TODO get annotation
+    // ArrayOutOfBounds
+    public VaultEntryType getVaultEntryType(int position) {
+            return entryTypeArray[position];
+    }
+
+    //
+    // SETTER
+    //
+    // TODO set time countdown
+    // ArrayOutOfBounds
+    public void setTimeCountDown(int position, int time) {
+            timeCountDownArray[position] = time;
+    }
+    // TODO set boolean
+    // ArrayOutOfBounds
+    public void setBoolean(int position, Boolean bool) {
+            booleanArray[position] = bool;
+    }
+    // TODO set annotation
+    // ArrayOutOfBounds
+    public void setVaultEntryType(int position, VaultEntryType annotaion) {
+            entryTypeArray[position] = annotaion;
+    }
+
+}
