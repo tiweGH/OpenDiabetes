45db4badb0b5bbe75cf1c3bafed945c3aa9a681e
Chryat1s <33789672+Chryat1s@users.noreply.github.com>
2018-01-13 23:36:47 +0100

    Created new test for BucketProcessor.

--- engine/test/de/jhit/opendiabetes/vault/processing/BucketProcessorTest.java
+++ engine/test/de/jhit/opendiabetes/vault/processing/BucketProcessorTest.java
@@ -22,8 +22,10 @@ import de.jhit.opendiabetes.vault.container.VaultEntry;
 import de.jhit.opendiabetes.vault.container.VaultEntryAnnotation;
 import de.jhit.opendiabetes.vault.container.VaultEntryType;
 import de.jhit.opendiabetes.vault.processing.filter.TestFunctions;
+import static de.jhit.opendiabetes.vault.util.TimestampUtils.addMinutesToTimestamp;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -38,6 +40,9 @@ import org.junit.Test;
  */
 public class BucketProcessorTest extends Assert {
     
+    List<BucketEntry> result;
+    BucketProcessor instance;
+            
     public BucketProcessorTest() {
     }
 
@@ -73,12 +78,46 @@ public class BucketProcessorTest extends Assert {
         }
     }
     
+    // 
+    // please ignore
+    // 
+    /*
+    @Test
+    public void testTimeCounter_one_minute_later() throws ParseException {
+        Date timeCounter = TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01");
+        Date timeCounter_2 = addMinutesToTimestamp(timeCounter, 1);
+        Date timeCounterWanted = TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02");
+        
+        assertEquals(timeCounter_2, timeCounterWanted);
+    }    
+    @Test
+    public void testTimeCounter_the_next_day() throws ParseException {
+        Date timeCounter = TestFunctions.creatNewDateToCheckFor("2000.01.01-23:59");
+        Date timeCounter_2 = addMinutesToTimestamp(timeCounter, 1);
+        Date timeCounterWanted = TestFunctions.creatNewDateToCheckFor("2000.01.02-00:00");
+        
+        assertEquals(timeCounter_2, timeCounterWanted);
+    }    
+    @Test
+    public void testTimeCounter_the_next_year() throws ParseException {
+        Date timeCounter = TestFunctions.creatNewDateToCheckFor("2000.12.31-23:59");
+        Date timeCounter_2 = addMinutesToTimestamp(timeCounter, 1);
+        Date timeCounterWanted = TestFunctions.creatNewDateToCheckFor("2001.01.01-00:00");
+        
+        assertEquals(timeCounter_2, timeCounterWanted);
+    }
+    */
+    // 
+    // please ignore
+    // 
+    
     @Test
     public void testBucketProcessor_1() throws ParseException {
         List<VaultEntry> vaultEntries = new ArrayList<>();        
         vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));
-        
-        List<BucketEntry> result = BucketProcessor.createListOfBuckets(vaultEntries);
+    
+        instance = new BucketProcessor();
+        result = instance.createListOfBuckets(vaultEntries);
         
         List<BucketEntry> wantedListOfBuckets = new ArrayList<>();
         wantedListOfBuckets.add(new BucketEntry(1, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
@@ -89,22 +128,66 @@ public class BucketProcessorTest extends Assert {
     }
     
     @Test
-    public void testBucketProcessor_2() throws ParseException {
+    public void testBucketProcessor_2_date_before() throws ParseException {
         List<VaultEntry> vaultEntries = new ArrayList<>();
-        List<VaultEntryAnnotation> tmpAnnotations;
         
         vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));
-        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03"), 182));
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 182));
         
-        List<BucketEntry> result = BucketProcessor.createListOfBuckets(vaultEntries);
+        instance = new BucketProcessor();
+        result = instance.createListOfBuckets(vaultEntries);
         
         List<BucketEntry> wantedListOfBuckets = new ArrayList<>();
         wantedListOfBuckets.add(new BucketEntry(1, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
         wantedListOfBuckets.get(0).setTimeCountDown(0, 1);
         wantedListOfBuckets.get(0).setOnehoteInformationArray(0, 1);
-        wantedListOfBuckets.add(new BucketEntry(3, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 182) ));
-        wantedListOfBuckets.get(2).setTimeCountDown(2, 1);
-        wantedListOfBuckets.get(2).setOnehoteInformationArray(2, 1);
+        wantedListOfBuckets.add(new BucketEntry(2, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 182) ));
+        wantedListOfBuckets.get(1).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(1).setOnehoteInformationArray(0, 1);
+        
+        testBucketInformation(result, wantedListOfBuckets);
+    }
+    
+    
+    @Test
+    public void testBucketProcessor_2_date_equal() throws ParseException {
+        List<VaultEntry> vaultEntries = new ArrayList<>();
+        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 182));
+        
+        instance = new BucketProcessor();
+        result = instance.createListOfBuckets(vaultEntries);
+        
+        List<BucketEntry> wantedListOfBuckets = new ArrayList<>();
+        wantedListOfBuckets.add(new BucketEntry(1, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
+        wantedListOfBuckets.get(0).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(0).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(2, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 182) ));
+        wantedListOfBuckets.get(1).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(1).setOnehoteInformationArray(0, 1);
+        
+        testBucketInformation(result, wantedListOfBuckets);
+    }
+    
+    @Test
+    public void testBucketProcessor_2_date_after() throws ParseException {
+        List<VaultEntry> vaultEntries = new ArrayList<>();
+        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03"), 182));
+        
+        instance = new BucketProcessor();
+        result = instance.createListOfBuckets(vaultEntries);
+        
+        List<BucketEntry> wantedListOfBuckets = new ArrayList<>();
+        wantedListOfBuckets.add(new BucketEntry(1, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
+        wantedListOfBuckets.get(0).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(0).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(2, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02")) ));
+        wantedListOfBuckets.add(new BucketEntry(3, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03"), 182) ));
+        wantedListOfBuckets.get(2).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(2).setOnehoteInformationArray(0, 1);
         
         testBucketInformation(result, wantedListOfBuckets);
     }
@@ -120,7 +203,8 @@ public class BucketProcessorTest extends Assert {
         vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_BG, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 109.0, tmpAnnotations));
         vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03"), 182));
         
-        //List<BucketEntry> result = BucketProcessor.createListOfBuckets(vaultEntries);
+        instance = new BucketProcessor();
+        result = instance.createListOfBuckets(vaultEntries);
         
         List<BucketEntry> wantedListOfBuckets = new ArrayList<>();
         wantedListOfBuckets.add(new BucketEntry(1, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
@@ -129,13 +213,94 @@ public class BucketProcessorTest extends Assert {
         tmpAnnotations = new ArrayList<>();
         tmpAnnotations.add(new VaultEntryAnnotation(VaultEntryAnnotation.TYPE.GLUCOSE_BG_METER_SERIAL).setValue("BG1140084B"));
         wantedListOfBuckets.add(new BucketEntry(2, new VaultEntry(VaultEntryType.GLUCOSE_BG, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 109.0, tmpAnnotations) ));
-        // wantedListOfBuckets.get(1).setTimeCountDown(1, 0);
-        // wantedListOfBuckets.get(1).setOnehoteInformationArray(1, 0);
+        // wantedListOfBuckets.get(1).setTimeCountDown(0, 0);
+        // wantedListOfBuckets.get(1).setOnehoteInformationArray(0, 0);
         wantedListOfBuckets.add(new BucketEntry(3, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03"), 182) ));
-        wantedListOfBuckets.get(2).setTimeCountDown(2, 1);
-        wantedListOfBuckets.get(2).setOnehoteInformationArray(2, 1);
+        wantedListOfBuckets.get(2).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(2).setOnehoteInformationArray(0, 1);
+        
+        testBucketInformation(result, wantedListOfBuckets);
+    }
+    
+    @Test
+    public void testBucketProcessor_4() throws ParseException {
+        List<VaultEntry> vaultEntries = new ArrayList<>();
+        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 170));        
+        vaultEntries.add(new VaultEntry(VaultEntryType.STRESS, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03"), 21,5));        
+        //
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:05"), 170));    
+        //
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:08"), 170));        
+        vaultEntries.add(new VaultEntry(VaultEntryType.STRESS, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:08"), 21,5));
+        
+        instance = new BucketProcessor();
+        result = instance.createListOfBuckets(vaultEntries);
+        
+        List<BucketEntry> wantedListOfBuckets = new ArrayList<>();
+        wantedListOfBuckets.add(new BucketEntry(1, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
+        wantedListOfBuckets.get(0).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(0).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(2, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
+        wantedListOfBuckets.get(1).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(1).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(3, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 170) ));
+        wantedListOfBuckets.get(2).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(2).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(4, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03")) ));
+        wantedListOfBuckets.add(new BucketEntry(5, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:04")) ));
+        wantedListOfBuckets.add(new BucketEntry(6, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:05"), 170) ));
+        wantedListOfBuckets.get(5).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(5).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(7, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:06")) ));
+        wantedListOfBuckets.add(new BucketEntry(8, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:07")) ));
+        wantedListOfBuckets.add(new BucketEntry(9, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:08"), 170) ));
+        wantedListOfBuckets.get(8).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(8).setOnehoteInformationArray(0, 1);
+        
+        testBucketInformation(result, wantedListOfBuckets);
+    }
+    
+    @Test
+    public void testBucketProcessor_5() throws ParseException {
+        List<VaultEntry> vaultEntries = new ArrayList<>();
+        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170));        
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 170));        
+        vaultEntries.add(new VaultEntry(VaultEntryType.STRESS, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03"), 21,5));        
+        //
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:05"), 170));    
+        //        
+        vaultEntries.add(new VaultEntry(VaultEntryType.STRESS, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:08"), 21,5));
+        vaultEntries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:08"), 170));
+        
+        //instance = new BucketProcessor();
+        //result = instance.createListOfBuckets(vaultEntries);
+        
+        List<BucketEntry> wantedListOfBuckets = new ArrayList<>();
+        wantedListOfBuckets.add(new BucketEntry(1, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
+        wantedListOfBuckets.get(0).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(0).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(2, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:01"), 170) ));
+        wantedListOfBuckets.get(1).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(1).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(3, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:02"), 170) ));
+        wantedListOfBuckets.get(2).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(2).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(4, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:03")) ));
+        wantedListOfBuckets.add(new BucketEntry(5, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:04")) ));
+        wantedListOfBuckets.add(new BucketEntry(6, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:05"), 170) ));
+        wantedListOfBuckets.get(5).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(5).setOnehoteInformationArray(0, 1);
+        wantedListOfBuckets.add(new BucketEntry(7, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:06")) ));
+        wantedListOfBuckets.add(new BucketEntry(8, new VaultEntry(VaultEntryType.EMPTY, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:07")) ));
+        wantedListOfBuckets.add(new BucketEntry(9, new VaultEntry(VaultEntryType.GLUCOSE_CGM_ALERT, TestFunctions.creatNewDateToCheckFor("2000.01.01-00:08"), 170) ));
+        wantedListOfBuckets.get(8).setTimeCountDown(0, 1);
+        wantedListOfBuckets.get(8).setOnehoteInformationArray(0, 1);
         
         //testBucketInformation(result, wantedListOfBuckets);
     }
-    
 }
