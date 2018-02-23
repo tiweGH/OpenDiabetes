ef59536f4aceb4119f1dbcf24f4e1476c5609eee
Chryat1s <33789672+Chryat1s@users.noreply.github.com>
2018-02-23 01:43:24 +0100

    Changed author name.

--- engine/test/de/jhit/opendiabetes/vault/processing/filter/TypeAbsenceFilterTest.java
+++ engine/test/de/jhit/opendiabetes/vault/processing/filter/TypeAbsenceFilterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 aa80hifa
+ * Copyright (C) 2017 a.a.aponte
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ import org.junit.Test;
 
 /**
  *
- * @author aa80hifa
+ * @author a.a.aponte
  */
 public class TypeAbsenceFilterTest extends Assert {
 
