71c18c170eadc5322a45a7a9ef058d1c43b620b8
tiwe <tiwe@172.18.80.223>
2018-03-28 15:24:54 +0200

    TimeStampUtilsTest erweitert

--- engine/test/de/opendiabetes/vault/util/TimestampUtilsTest.java
+++ engine/test/de/opendiabetes/vault/util/TimestampUtilsTest.java
@@ -20,8 +20,10 @@ import de.opendiabetes.vault.util.TimestampUtils;
 import de.opendiabetes.vault.container.VaultEntry;
 import de.opendiabetes.vault.container.VaultEntryType;
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import javafx.util.Pair;
@@ -256,6 +258,143 @@ public class TimestampUtilsTest {
 
         date = TimestampUtils.createCleanTimestamp("2017.06.29-05:00", "yyyy.MM.dd-HH:mm");
         assertTrue(TimestampUtils.withinTimeSeries(timeSeries, date));
+    }
+
+    @Test
+    public void testCreateCleanTimestamp() throws ParseException {
+        Date testDate = TimestampUtils.createCleanTimestamp("2018.03.28-04:56", "yyyy.MM.dd-HH:mm");
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 04, 56, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        Date expectedResult = cal.getTime();
+
+        assertEquals(expectedResult, testDate);
+
+        testDate = TimestampUtils.createCleanTimestamp("2018.03.28-04:56");
+
+        cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 04, 56, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        expectedResult = cal.getTime();
+
+        assertEquals(expectedResult, testDate);
+
+    }
+
+    @Test
+    public void testFromLocalDate() {
+        LocalDate localDate = LocalDate.of(2018, 3, 28);
+        Date testDate = TimestampUtils.fromLocalDate(localDate, 100);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 0, 0, 0);
+        cal.set(Calendar.MILLISECOND, 100);
+
+        Date expectedResult = cal.getTime();
+
+        assertEquals(expectedResult, testDate);
+
+        localDate = LocalDate.of(2018, 3, 28);
+        testDate = TimestampUtils.fromLocalDate(localDate);
+
+        cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 0, 0, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        expectedResult = cal.getTime();
+
+        assertEquals(expectedResult, testDate);
+    }
+
+    @Test
+    public void testDateToLocalDate() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 4, 50, 0);
+        cal.set(Calendar.MILLISECOND, 100);
+
+        LocalDate expectedResult = LocalDate.of(2018, 3, 28);
+        LocalDate testDate = TimestampUtils.dateToLocalDate(cal.getTime());
+
+        assertEquals(expectedResult, testDate);
+    }
+
+    @Test
+    public void testDateToLocalTime() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 4, 50, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        LocalTime expectedResult = LocalTime.of(4, 50, 0);
+        LocalTime testTime = TimestampUtils.dateToLocalTime(cal.getTime());
+
+        assertEquals(expectedResult, testTime);
+    }
+
+    @Test
+    public void testTimestampToString() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 4, 50, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        String expectedResult = "2018.03.28-04:50";
+        String testString = TimestampUtils.timestampToString(cal.getTime(), TimestampUtils.TIME_FORMAT_DATASETS);
+
+        assertEquals(expectedResult, testString);
+    }
+
+    @Test
+    public void testGetHourOfDay() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 4, 50, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        Integer expectedResult = 4;
+        Integer testVal = TimestampUtils.getHourOfDay(cal.getTime());
+
+        assertEquals(expectedResult, testVal);
+    }
+
+    @Test
+    public void testMinuteOfHour() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2018, 02, 28, 4, 50, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        Integer expectedResult = 50;
+        Integer testVal = TimestampUtils.getMinuteOfHour(cal.getTime());
+
+        assertEquals(expectedResult, testVal);
+    }
+
+    @Test
+    public void testWithinTimeSpan() throws ParseException {
+        Date start = TimestampUtils.createCleanTimestamp("2018.03.28-00:00");
+        Date end = TimestampUtils.createCleanTimestamp("2018.03.28-12:00");
+        Date date1 = TimestampUtils.createCleanTimestamp("2018.03.28-04:50");
+        Date date2 = TimestampUtils.createCleanTimestamp("2018.03.28-13:00");
+
+        assertTrue(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), date1));
+        assertTrue(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), TimestampUtils.dateToLocalTime(date1)));
+
+        assertFalse(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), date2));
+        assertFalse(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), TimestampUtils.dateToLocalTime(date2)));
+    }
+
+    @Test
+    public void testWithinTimeSpan_MultipleDays() throws ParseException {
+        Date start = TimestampUtils.createCleanTimestamp("2018.03.28-23:00");
+        Date end = TimestampUtils.createCleanTimestamp("2018.03.29-12:00");
+        Date date1 = TimestampUtils.createCleanTimestamp("2018.03.29-04:50");
+        Date date2 = TimestampUtils.createCleanTimestamp("2018.03.30-13:00");
+
+        assertTrue(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), date1));
+        assertTrue(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), TimestampUtils.dateToLocalTime(date1)));
+
+        assertFalse(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), date2));
+        assertFalse(TimestampUtils.withinTimeSpan(TimestampUtils.dateToLocalTime(start), TimestampUtils.dateToLocalTime(end), TimestampUtils.dateToLocalTime(date2)));
     }
 
 }
